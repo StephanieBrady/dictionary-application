c53252f64f87e3fd99b9f6c352efbd86d2b70306
StephanieBrady <stephaniebrady1929@gmail.com>
2022-10-25 11:18:21 -0400

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -8,6 +8,7 @@ export default function Results(props) {
       <div className="Results">
         <br />
         <h2>{props.results.word}</h2>
+        Phonetics:
         {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
