6e91a36f3166edd7066e8705dfdc5cebfe0c2f6d
StephanieBrady <stephaniebrady1929@gmail.com>
2022-10-27 09:09:14 -0400

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
-import Results from "./Results.css";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
@@ -12,7 +12,7 @@ export default function Results(props) {
           <h2>{props.results.word}</h2>
           Phonetics:
           {props.results.phonetics
-            .filter((phonetic) => phonetic.audio != "")
+            .filter((phonetic) => phonetic.audio !== "")
             .map(function (phonetic, index) {
               return (
                 <div key={index}>
